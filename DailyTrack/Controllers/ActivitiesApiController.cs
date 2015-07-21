6ad31c505fce2cd991f4498503e8e32fbd11fb45
gommjabbar <gommjabbar@gmail.com>
2015-07-21 11:07:09 +0300

    ApiController + jquery stuff

---
+++ DailyTrack/Controllers/ActivitiesApiController.cs
@@ -0,0 +1,31 @@
+﻿using DailyTrack.Models;
+using DailyTrack.Repos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DailyTrack.Controllers
+{
+    [RoutePrefix("api/activities")]
+    public class ActivitiesApiController : ApiController
+    {
+        private IActivityRepository activityRepository;
+
+        public ActivitiesApiController()
+        {
+            activityRepository = new ActivityRepository(new DailyTrackDbContext());
+        }
+
+        [HttpPost]
+        [Route("")]
+        public bool InsertActivity(Activity activity)
+        {
+            activityRepository.InsertActivity(activity);
+            activityRepository.Save();
+            return true;
+        }
+    }
+}
