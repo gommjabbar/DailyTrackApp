2847e6872253202d53b1c167aeb0f616c4bb16a4
ferentedi <ferent_edi@yahoo.com>
2015-07-24 12:50:43 +0300

    folder-list-component

---
+++ DailyTrack/Scripts/components/folder-list-component.js
@@ -0,0 +1,26 @@
+﻿ko.components.register('folder-list', {
+    viewModel: function (params) {
+        var self = this;
+        self.Folders = ko.observableArray();
+        self.FolderChange = params.FolderChange;
+        self.FolderChange.subscribe(function () {
+            self.fnGetFolders();
+        })
+
+        self.fnGetFolders = function () {
+            $.ajax({
+                url: "/api/folders",
+                method: "GET",
+                data: {
+                }
+            }).done(function (data) {
+                var result = $.map(data, function (item, index) {
+                    return new Folder(item);
+                });
+                self.Folders(result);
+            })
+        }
+        self.fnGetFolders();
+    },
+    template: { fromFileType: 'html' }
+});
