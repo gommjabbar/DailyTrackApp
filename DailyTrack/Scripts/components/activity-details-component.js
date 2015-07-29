ff6ae59365f29f3aeb691f040334d1146cb3bcff
ferentedi <ferent_edi@yahoo.com>
2015-07-29 16:39:58 +0300

    aspect fixes2

--- DailyTrack/Scripts/components/activity-details-component.js
+++ DailyTrack/Scripts/components/activity-details-component.js
@@ -2,6 +2,16 @@
     viewModel: function (params) {
         var self = this;
         self.SelectedActivity = params.SelectedActivity;
+
+        //The function removes an activity
+        self.fnRemoveActivity = function () {
+            $.ajax({
+                url: "/api/activities/" + self.SelectedActivity().id,
+                method: "Delete",
+                async: false,
+            }).done(function (result) {
+            })
+        }
     },
     template: { fromFileType: 'html' }
 });
